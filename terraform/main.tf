61ae8de3ee175f349ab381f67e7e162780b64e4d
root <root@ip-172-31-37-225.us-east-2.compute.internal>
2019-08-04 09:59:14 +0000

    Final IaC data

---
+++ terraform/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+        access_key = "AKIAUYM3AHVLLCGNXWUN"
+        secret_key = "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"
+        region = "us-east-2"
+}
+
+
+# Creating the Security Group !!
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+# Creating ec2 instance with
+resource "aws_instance" "casestudy" {
+        ami = "ami-02f706d959cedf892"
+        instance_type = "t2.micro"
+        tags = { Name: "casestudy",
+		 Role = "app"
+		}
+        key_name = "shaan-rewind"
+        availability_zone = "us-east-2a"
+        security_groups = ["${aws_security_group.allow_all.name}"]
+        root_block_device {
+                volume_size = 20
+        }
+}
+
+# Attaching 100GB volume to the Ec2 instance
+
+resource "aws_volume_attachment" "tasc-volume" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.tasc-ebs.id
+  instance_id = "${aws_instance.casestudy.id}"
+}
+
+# Creating the ebs volume with 100GB size
+
+resource "aws_ebs_volume" "tasc-ebs" {
+  availability_zone = "us-east-2a"
+  size              = 100
+  tags = {
+    Name = "Ebs-Volume"
+  }
+}
+
